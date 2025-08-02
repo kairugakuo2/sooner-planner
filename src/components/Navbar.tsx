6cef3180c211efb4ab0601130603b899d8ecf032
Gakuo <kairugakuo9@gmail.com>
2025-08-02 03:24:20 +0300

    imported shadcn and fixed some css overiding from importing

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,14 +4,18 @@ import React from 'react';
 
 const Navbar = () => {
     return (
-        <nav className="flex justify-between items-center px-6 py-4">
+        <nav className="flex justify-between items-center px-6 py-4 sticky top-0 z-50 bg-white">
             <div className="flex items-center space-x-6 ">
-                <div className="text-2xl font-bold text-red-600 cursor-pointer">
-                    Sooner Planner
-                </div>
-                <div className="cursor-pointer ">
-                    How It Works
-                </div>
+                <Link href="/">
+                    <span className="text-2xl font-bold text-red-600 cursor-pointer">
+                        Sooner Planner
+                    </span>
+                </Link>
+                <Link href="/how-it-works">
+                    <span className="cursor-pointer hover:text-red-700 transition-colors ">
+                        How It Works
+                    </span>
+                </Link>
             </div>
             <a 
                 href="https://github.com/kairugakuo2/sooner-planner" 
